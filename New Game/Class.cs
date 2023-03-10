2e937ad24200cac8d1ea2182d4be08693dd91dd5
NecromancerMidas <115973453+NecromancerMidas@users.noreply.github.com>
2023-03-10 15:05:00 +0100

    Lil Rpg Thingy

---
+++ New Game/Class.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+
+namespace Classes {
+    
+    
+    public class WarriorClass
+    
+    {
+
+     public int Health = 2000;
+        public int Mana = 500;
+        public int Strength = 200;
+    
+
+
+}
+    public class MageClass
+
+    {
+
+        public int Health = 2000;
+       public int Mana = 500;
+       public int Strength = 200;
+
+
+
+    }
+    public class RogueClass
+
+    {
+
+        public int Health = 2000;
+        public int Mana = 500;
+        public int Strength = 200;
+
+
+
+    }
+    public class CurrentClass
+    {
+        public int Health = 2000;
+        public int Mana = 500;
+        public int Strength = 200;
+
+
+
+    }
+
+
+ 
+
+}
---
+++ New Game/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+
+using System;
+using Classes;
+
+namespace Rpg
+{
+
+    class MainGame
+    {
+
+
+        static void Main(string[] args)
+        {
+            var Class = new CurrentClass();
+            Console.WriteLine("Hello Midas");
+            Console.WriteLine("hello again");
+            Console.WriteLine();
+            ChooseClass(Class);
+            Console.WriteLine(Class.Health + " " + Class.Mana + " " + Class.Strength);
+
+
+        }
+
+        public static void ChooseClass(CurrentClass ChosenClass)
+        {
+            ConsoleKeyInfo Class = Console.ReadKey(true);
+
+            if (Class.Key == ConsoleKey.W)
+            {
+                var war = new WarriorClass();
+                Console.WriteLine("You have chosen Warrior");
+                ChosenClass.Health = war.Health;
+                ChosenClass.Mana = war.Mana;
+                ChosenClass.Strength = war.Strength;
+            }
+            else if (Class.Key == ConsoleKey.M)
+            {
+                Console.WriteLine("You have chosen Mage");
+
+            }
+            else
+            {
+                Console.WriteLine("You have chosen Rogue");
+
+            }
+
+        }
+
+    }
+}
