7d19d41c22fd8363a1eb5a89583e8bd486eae97a
NecromancerMidas <115973453+NecromancerMidas@users.noreply.github.com>
2023-03-11 13:16:10 +0100

    Intro better key consistency

--- New Game/Program.cs
+++ New Game/Program.cs
@@ -4,6 +4,7 @@
 
 
 using System;
+using System.Data;
 using Classes;
 
 namespace Rpg
@@ -13,14 +14,43 @@ namespace Rpg
     {
 
 
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.SetWindowSize(200, 40);
+            Console.Title = "My Game";
+            Console.ForegroundColor = ConsoleColor.Magenta;
             var Class = new CurrentClass();
-            Console.WriteLine("Hello Midas");
-            Console.WriteLine("hello again");
-            Console.WriteLine();
-            ChooseClass(Class);
-            Console.WriteLine(Class.Health + " " + Class.Mana + " " + Class.Strength);
+            Console.WriteLine(@"
+ __       __  __        __                      __               ______         __                                 __                                        
+/  \     /  |/  |      /  |                    /  |             /      \       /  |                               /  |                                       
+$$  \   /$$ |$$/   ____$$ |  ______    _______ $$/______       /$$$$$$  |  ____$$ | __     __ ______   _______   _$$ |_    __    __   ______   ______        
+$$$  \ /$$$ |/  | /    $$ | /      \  /       |$/       |      $$ |__$$ | /    $$ |/  \   /  /      \ /       \ / $$   |  /  |  /  | /      \ /      \       
+$$$$  /$$$$ |$$ |/$$$$$$$ | $$$$$$  |/$$$$$$$/ /$$$$$$$/       $$    $$ |/$$$$$$$ |$$  \ /$$/$$$$$$  |$$$$$$$  |$$$$$$/   $$ |  $$ |/$$$$$$  /$$$$$$  |      
+$$ $$ $$/$$ |$$ |$$ |  $$ | /    $$ |$$      \ $$      \       $$$$$$$$ |$$ |  $$ | $$  /$$/$$    $$ |$$ |  $$ |  $$ | __ $$ |  $$ |$$ |  $$/$$    $$ |      
+$$ |$$$/ $$ |$$ |$$ \__$$ |/$$$$$$$ | $$$$$$  | $$$$$$  |      $$ |  $$ |$$ \__$$ |  $$ $$/ $$$$$$$$/ $$ |  $$ |  $$ |/  |$$ \__$$ |$$ |     $$$$$$$$/       
+$$ | $/  $$ |$$ |$$    $$ |$$    $$ |/     $$/ /     $$/       $$ |  $$ |$$    $$ |   $$$/  $$       |$$ |  $$ |  $$  $$/ $$    $$/ $$ |     $$       |      
+$$/      $$/ $$/  $$$$$$$/  $$$$$$$/ $$$$$$$/  $$$$$$$/        $$/   $$/  $$$$$$$/     $/    $$$$$$$/ $$/   $$/    $$$$/   $$$$$$/  $$/       $$$$$$$/ ");
+            Console.WriteLine(@"
+Welcome to Midas's Adventure, a small rpg game I created for fun. 
+Please press your Enter key to start");
+            ConsoleKeyInfo ifEnter = Console.ReadKey(true);
+            if (ifEnter.Key == ConsoleKey.Enter)
+            {
+                Console.WriteLine(@"
+Please choose a class. Your options 
+W: Warrior
+M: Mage
+R: Rogue
+Choose Wisely
+");
+                ChooseClass(Class);
+                Console.WriteLine(Class.Health + " " + Class.Mana + " " + Class.Strength);
+            }
+            else {
+                Console.Clear();
+                Main();
+            }
+
 
 
         }
@@ -39,13 +69,25 @@ namespace Rpg
             }
             else if (Class.Key == ConsoleKey.M)
             {
+                var mag = new MageClass();
                 Console.WriteLine("You have chosen Mage");
+                ChosenClass.Health = mag.Health;
+                ChosenClass.Mana = mag.Mana;
+                ChosenClass.Strength = mag.Strength;
 
+            }
+            else if (Class.Key == ConsoleKey.R)
+            {
+                var rog = new RogueClass();
+                Console.WriteLine("You have chosen Rogue");
+                ChosenClass.Health = rog.Health;
+                ChosenClass.Mana = rog.Mana;
+                ChosenClass.Strength = rog.Strength;
             }
             else
             {
-                Console.WriteLine("You have chosen Rogue");
-
+                Console.WriteLine("You have to choose a class. W: Warrior, M: Mage, R: Rogue.");
+                ChooseClass(ChosenClass);
             }
 
         }
