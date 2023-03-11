71c740c57185e9870c44bb04d7fec1bca76646d4
NecromancerMidas <115973453+NecromancerMidas@users.noreply.github.com>
2023-03-11 16:50:08 +0100

    Cleaned up code in Main

--- New Game/Program.cs
+++ New Game/Program.cs
@@ -17,7 +17,7 @@ namespace Rpg
         static void Main()
         {
             Console.SetWindowSize(200, 40);
-            Console.Title = "My Game";
+            Console.Title = "Midas's Adventure";
             Console.ForegroundColor = ConsoleColor.Magenta;
             var Class = new CurrentClass();
             Console.WriteLine(@"
@@ -43,52 +43,18 @@ M: Mage
 R: Rogue
 Choose Wisely
 ");
-                ChooseClass(Class);
-                Console.WriteLine(Class.Health + " " + Class.Mana + " " + Class.Strength);
+                ClassChooser.ChooseClass(Class);
+                Console.WriteLine("Please give yourself a name I recommend Midas its a good name.");
+                Class.Name = Console.ReadLine() + " the " + Class.Class;
+                Stats.DrawStats(Class);
             }
-            else {
+            else
+            {
                 Console.Clear();
                 Main();
             }
 
 
-
-        }
-
-        public static void ChooseClass(CurrentClass ChosenClass)
-        {
-            ConsoleKeyInfo Class = Console.ReadKey(true);
-
-            if (Class.Key == ConsoleKey.W)
-            {
-                var war = new WarriorClass();
-                Console.WriteLine("You have chosen Warrior");
-                ChosenClass.Health = war.Health;
-                ChosenClass.Mana = war.Mana;
-                ChosenClass.Strength = war.Strength;
-            }
-            else if (Class.Key == ConsoleKey.M)
-            {
-                var mag = new MageClass();
-                Console.WriteLine("You have chosen Mage");
-                ChosenClass.Health = mag.Health;
-                ChosenClass.Mana = mag.Mana;
-                ChosenClass.Strength = mag.Strength;
-
-            }
-            else if (Class.Key == ConsoleKey.R)
-            {
-                var rog = new RogueClass();
-                Console.WriteLine("You have chosen Rogue");
-                ChosenClass.Health = rog.Health;
-                ChosenClass.Mana = rog.Mana;
-                ChosenClass.Strength = rog.Strength;
-            }
-            else
-            {
-                Console.WriteLine("You have to choose a class. W: Warrior, M: Mage, R: Rogue.");
-                ChooseClass(ChosenClass);
-            }
 
         }
 
