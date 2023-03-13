0d006f958db52736a7f1445d3cc78afc8dce3bf3
NecromancerMidas <115973453+NecromancerMidas@users.noreply.github.com>
2023-03-13 22:06:01 +0100

    Added randomizer

---
+++ New Game/Randomizer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Randomer
+{
+	public static int Randomizer(int min, int max)
+	{
+		Random rng = new Random();
+	    int num = rng.Next(min, max);
+		return num;
+	  
+
+	}
+}
