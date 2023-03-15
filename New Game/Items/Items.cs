9b349a9a9991721bcf86584a268f0d8892c4f18b
NecromancerMidas <115973453+NecromancerMidas@users.noreply.github.com>
2023-03-15 21:31:08 +0100

    Really have to drink doing this?

---
+++ New Game/Items/Items.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace New_Game.Items
+{
+
+
+    public class Items
+    {
+        public string Type { get; protected set; }
+        public int level { get; protected set; }
+        public string Rarity { get; protected set; }
+        public string Name { get; protected set; }
+        public string Description { get; protected set; }
+        public int WeaponStrength { get; protected set; }
+
+        public Items()
+        {
+
+
+        
+    }
+
+}
+}
+
+
